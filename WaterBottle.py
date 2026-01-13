ef0f1c430543398e844fa311cc82efbc23eb3c3f
kulwinder9115 <kulwinder09115@gmail.com>
2026-01-13 21:17:55 +0530

    Solution Leetcode 1518: Water Bottle

---
+++ WaterBottle.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numWaterBottles(self, numBottles, numExchange):
+        ans = numBottles
+        while (numBottles>=numExchange ):
+            newBottles = numBottles// numExchange
+            remBottles = numBottles % numExchange
+            ans= ans + newBottles
+            numBottles = newBottles+ remBottles
+        return ans
+
+# Example usage:
+numBottles = 9  
+numExchange = 3
+solution = Solution()
+print(solution.numWaterBottles(numBottles, numExchange))  # Output: 13
